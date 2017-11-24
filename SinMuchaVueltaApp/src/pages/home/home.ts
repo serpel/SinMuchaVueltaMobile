06ac2f83ef06019319076a0e7f99e990be7d7ede
Sergio Peralta <serpel.js@gmail.com>
2017-11-24 10:44:54 -0600

    some changes

--- SinMuchaVueltaApp/src/pages/home/home.ts
+++ SinMuchaVueltaApp/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController) { 
   }
+  
+  //constructor(public navCtrl: NavController, private iab: InAppBrowser) {   
+  //}
+  
 
+  //ngOnInit(){
+  //      const browser = this.iab.create('https://www.sinmuchavuelta.com','_system',{location:'no'}); 
+  //  }
+
+    
 }
